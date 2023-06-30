3366995fa2c73a92c92e10714525c012814dfc39
Youssef <youssefkaroui6@gmail.comm>
2023-06-30 17:46:26 -0400

    html 5-day-forecast updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,5 @@
 var apiKey="86dbb0be58935b576e4fdd7365e04545";
-var cityName="London"
+
 var requestUrlCityName="https://api.openweathermap.org/data/2.5/forecast?q=" + cityName + "&appid=" + apiKey ;
 
 function getWeatherInfo () {
