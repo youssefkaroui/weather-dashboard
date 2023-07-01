0e52e76eca74c802619fd1bc19a0adce4d0747a2
Youssef <youssefkaroui6@gmail.comm>
2023-07-01 14:21:05 -0400

    function displayforecast added and worksgit add . some css changes to the forecast-container

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,10 +4,11 @@ var apiKey="86dbb0be58935b576e4fdd7365e04545";
 
 // var requestUrl="https://api.openweathermap.org/geo/1.0/direct?q=" + CityEl+ "&appid=" + apiKey;
 var searchHistoryEl= $("#search-history-container");
-var forcastEl= $("#5-days-forcast");
+var forcastEl= $("#5-day-forecast");
 var searchInputEl= $("#search-place-holder");
 var searchBtn= $("#search-button");
 var CityEl= $("#city");
+var currentCityEl= $("#current-city")
 var DateEl= $("#current-date");
 var WeatherIconEl=$("#weather-icon");
 var temperatureEl=$("#temperature");
@@ -16,12 +17,11 @@ var humidityEl=$("#current-humidity");
 
 
 
-
+//gets the current weather and displays in html
 
 function getWeather(cityName){
-    
-    
-    var requestCurrent =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${apiKey}`
+
+    var requestCurrent =`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&units=imperial&appid=${apiKey}`
     fetch(requestCurrent)   
  .then(function(response){
      return response.json();
@@ -29,19 +29,17 @@ function getWeather(cityName){
  })
  .then(function(data){
     //  console.log(data);
-     CityEl.text($("#city"));
+    currentCityEl.text(cityName);
      DateEl.text(dayjs().format('dddd, MMM DD, YYYY'))
+     
      WeatherIconEl.attr("src","https://openweathermap.org/img/wn/"+ data.weather[0].icon+ "@2x.png");
-     temperatureEl.text("Temprature: " + data.main.temp + " \u00B0F");
+     temperatureEl.text("Temprature: " + data.main.temp + "\u00B0F");
      windEl.text("Wind speed: " + data.wind.speed + " MPH");
      humidityEl.text("Humidity: "+ data.main.humidity + "%" );
-     
- 
  })
-
 }
 
-
+// event listener for user input
 
 searchInputEl.on("submit",function(event){
     event.preventDefault();
@@ -51,23 +49,55 @@ searchInputEl.on("submit",function(event){
      getForecast(cityName);
 })
 
-
+// gets the data for 5 days forecast and calls a function to display information 
 function getForecast(cityName){
-    var requestForecast =`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&appid=${apiKey}`
+    var requestForecast =`https://api.openweathermap.org/data/2.5/forecast?q=${cityName}&units=imperial&appid=${apiKey}`
     fetch(requestForecast)   
  .then(function(response){
      return response.json();
 
  })
  .then(function(data){
-    //  console.log(data);
-     displayForecast();     
-     
- 
+      console.log(data);
+     displayForecast(data);     
  })
-
-
 }
-function displayForecast(){
-    
+function displayForecast(data){
+var forecastTitle=document.createElement("h3");
+forecastTitle.textContent= data.city.name + "  5-day-forcast";
+ $(forcastEl).append(forecastTitle);
+
+ for (i=5; i<data.list.length; i+=8) {
+     var forecastCard= document.createElement("div");
+     forecastCard.setAttribute("class","weather-card col"); 
+     $(forcastEl).append(forecastCard);
+
+     var forecastDate = document.createElement("p");
+        forecastDate.style.fontSize = "18px";
+        forecastDate.textContent = dayjs.unix(data.list[i].dt).format('dddd, MMM DD, YYYY');
+        
+        $(forecastCard).append(forecastDate);
+
+        var forecastIcon = document.createElement("img");
+        forecastIcon.setAttribute("src", "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon + "@2x.png");
+       
+        $(forecastCard).append(forecastIcon);
+
+        var forecastTemp = document.createElement("p");
+        forecastTemp.textContent = "Temp: " + data.list[i].main.temp + "\u00B0F";
+        
+        $(forecastCard).append(forecastTemp);
+
+        var forecastWind = document.createElement("p");
+        forecastWind.textContent = "Wind: " + data.list[i].wind.speed + " MPH";
+        $(forecastCard.append(forecastWind));
+
+        var forecastHumidity = document.createElement("p");
+        forecastHumidity.textContent = "Humidity: " + data.list[i].main.humidity + "%";
+       
+        $(forecastCard).append(forecastHumidity);
+
+
+ }
+
 }
